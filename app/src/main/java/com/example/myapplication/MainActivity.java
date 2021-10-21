bdb54cd1f379cba76025bac435420041556856f2
ionic93 <rhsfpem41@gmail.com>
2021-10-21 16:43:17 +0900

    2021/10/21

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
         btnConfirm = findViewById(R.id.btnConfirm); //view에 있는 버튼을 지정 => 변수가 초기화
         btnFrame = findViewById(R.id.btnFrame);
         btnTable = findViewById(R.id.btnTable);
+
 
         btnConfirm.setOnClickListener(v->{
 //            Toast.makeText(getApplicationContext(), "Hello Andriod", Toast.LENGTH_SHORT).show();
